34afcd760e3f0733f200c4d25537686870d6c910
soobeen27 <youdew27@icloud.com>
2023-01-03 16:04:44 +0900

    Initial Commit

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Soo Jang on 2023/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
